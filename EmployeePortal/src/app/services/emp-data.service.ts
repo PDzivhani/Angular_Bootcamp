298eea6cbbf5c10eab2e857d8cdaff388094f0e1
PDzivhani <dzivhanip0505@gmail.com>
2024-04-23 14:14:06 +0200

    Set up, styling, components,routing, forms, data binding

---
+++ EmployeePortal/src/app/services/emp-data.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+//import hhttpClient
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpDataService {
+
+  //import http client because json file acts as our server
+
+
+  //API = private url= ''
+
+
+//fake api
+  // constructor(private http: httpClient) { }
+  /* new fucnction get employee
+  getEmployees(): Observable<any>
+  {
+    return this.http.get<any>(this.Url);
+  }
+  
+  */
+
+
+}
